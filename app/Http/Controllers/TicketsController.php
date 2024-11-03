16f97e82c5f78e2070975da2f6f37c6ea406c0f2
salache <lfsalache@gmail.com>
2024-11-03 09:27:59 -0300

    update 03/11 - 2
    TicketsController

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -2,17 +2,64 @@
 
 namespace App\Http\Controllers;
 
-use App\Tickets;
+use App\Models\Tickets;
 use Illuminate\Http\Request;
 
 class TicketsController extends Controller
 {
-    public function tickets(Request $request)
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        $tickets = Tickets::select('id', 'subject', 'recipients', 'status', 'created_at', 'updated_at')->get();
+        //
+    }
 
-        return inertia('Dashboard', [
-            'tickets' => $tickets,
-        ]);
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tickets $tickets)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tickets $tickets)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Tickets $tickets)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tickets $tickets)
+    {
+        //
     }
 }
